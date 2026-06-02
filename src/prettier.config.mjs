c3e9a58eef7b2df7f752d90de75224272076defa
Mrfroxi <pilutikas340@gmail.com>
2026-06-02 13:02:11 +0300

    feat:init package

---
+++ src/prettier.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import("prettier").Config} */
+export default {
+    trailingComma: "none",
+    tabWidth: 4,
+    useTabs: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    arrowParens: "avoid",
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    importOrderCaseInsensitive: true,
+    importOrderParserPlugins: [
+        "classProperties",
+        "decorators-legacy",
+        "typescript"
+    ],
+    importOrder: ["<THIRD_PARTY_MODULES>", "^@/(.*)$", "^../(.*)", "^./(.*)"],
+    plugins: ["@trivago/prettier-plugin-sort-imports"]
+}
